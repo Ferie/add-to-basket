3bc7c435729f8096c961de3bdd8c0435333c0369
Riccardo <riccardo.andreatta@hotmail.com>
2020-07-09 15:02:39 +0100

    package.json reviewed for heroku deploy

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static(__dirname + '/dist'));
+
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
+// If an incoming request uses a protocol other than HTTPS,
+// redirect that request to the same url but with HTTPS
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+
+// Instruct the server to use the forceSSL middleware
+server.use(forceSSL());
+
+server.listen(process.env.PORT || 8080, () => {
+    console.log('\x1b[36m%s\x1b[0m', 'Server running on port', (process.env.PORT || 8080));
+});
