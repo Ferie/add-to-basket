f4fdefaa8cab85692a2f31ec935405a49fa1a327
Riccardo <riccardo.andreatta@hotmail.com>
2020-07-15 16:57:13 +0100

    Added redirect path for anything else different from the expected ones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
         component: DetailsPageComponent,
         data: {},
         resolve: {}
+    },
+    {
+        path: '**',
+        redirectTo: '/'
     }
 ];
 
